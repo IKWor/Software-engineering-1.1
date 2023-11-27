b09243ac739f059a6d16389a86b6d2d0fc7150eb
AGBullet <78823503+AGBullet@users.noreply.github.com>
2023-11-27 23:13:19 +0300

    Update test_main.py

--- HW3/bullet/test_main.py
+++ HW3/bullet/test_main.py
@@ -8,7 +8,8 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
-    
+
+
 def test_detector_photo():
     with TestClient(app) as client:
         url = "/get_detector/"
