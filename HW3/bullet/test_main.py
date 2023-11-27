58fc6915209a254787e331bc5e41ba014b4f2dc8
AGBullet <denism3melnikov@list.ru>
2023-11-27 20:56:03 +0300

    Added test_main.py

---
+++ HW3/bullet/test_main.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+    
+
+def test_detector_photo():
+    with TestClient(app) as client:
+        url = "/get_detector/"
+        response = client.get(url)
+        assert response.status_code == 200
+  
