12390a6e79e920781a932e90918560de1a516f81
IKWor <149700414+IKWor@users.noreply.github.com>
2023-12-13 23:43:07 +0300

    Update test_main.py

--- project/test_main.py
+++ project/test_main.py
@@ -8,3 +8,19 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_predict_positive():
+    response = client.post("/predict/",
+                           json={"text": "I like ML!"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['label'] == 'POSITIVE'
+
+
+def test_predict_negative():
+    response = client.post("/predict/",
+                           json={"text": "I hate ML!"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['label'] == 'NEGATIVE'
