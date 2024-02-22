aa23876666a78dba2dc3eaaa20c733aa450ccd70
AGBullet <78823503+AGBullet@users.noreply.github.com>
2024-02-22 20:03:44 +0300

    Update main.py

--- HW3/bullet/main.py
+++ HW3/bullet/main.py
@@ -1,20 +1,24 @@
 from fastapi import FastAPI
-from transformers import pipeline 
+from transformers import pipeline
 from pydantic import BaseModel
 
 app = FastAPI()
 detector = pipeline(model="facebook/detr-resnet-50")
 
+
 class Item(BaseModel):
     text: str
+
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
+
 @app.get("/get_detector/")
 def getDetector():
     return detector("https://huggingface.co/datasets/mishig/sample_images/resolve/main/airport.jpg")
+
 
 @app.post("/detector/")
 def detect(item: Item):
